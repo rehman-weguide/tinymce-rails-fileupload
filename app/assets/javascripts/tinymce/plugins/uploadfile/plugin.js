c839bbaa2e66c07906dcde24c28e1aec66a5b1ab
Muhammad ur Rehman <142472093+rehman-weguide@users.noreply.github.com>
2024-07-23 17:10:11 +0500

    Update plugin.js

--- app/assets/javascripts/tinymce/plugins/uploadfile/plugin.js
+++ app/assets/javascripts/tinymce/plugins/uploadfile/plugin.js
@@ -1,137 +1,216 @@
-tinymce.PluginManager.add('AwsS3Upload', (editor, url)=> {
+(function() {
+  tinymce.PluginManager.requireLangPack('uploadfile');
 
-    //Grab the params from TinyMCE init
-    const {bucketName, folderName = '', awsAuth, buttonText = 'Upload File', progress, secondFileSelectedBeforeFirstUpload} = editor.getParam('Awss3UploadSettings');
-    let inProgress = false;
+  tinymce.create('UploadFile', {
+    UploadFile: function(ed, url) {
+      var form,
+          iframe,
+          win,
+          editor = ed;
 
-    //Initializing parameters control
-    if (!bucketName) {
-        console.log('`bucketName` parameter missing on init AwsS3Upload TinyMCE plugin.');
-        return false;
-    }
-    //awsAuth control
-    if (awsAuth && typeof awsAuth === 'object') {
-        const {secretAccessKey, accessKeyId, region} = awsAuth;
-        if (!accessKeyId) {
-            console.log('`awsAuth` parameter missing `accessKeyId` property on init AwsS3Upload TinyMCE plugin.');
-            return false;
-        }
-        if (!secretAccessKey) {
-            console.log('`awsAuth` parameter missing `secretAccessKey` property on init AwsS3Upload TinyMCE plugin.');
-            return false;
-        }
-        if (!region) {
-            console.log('`awsAuth` parameter missing `secretAccessKey` property on init AwsS3Upload TinyMCE plugin.');
-            return false;
-        }
-        //Ok let's update the auth config
-        AWS.config.region = region;
-        AWS.config.update({
-            accessKeyId: accessKeyId,
-            secretAccessKey: secretAccessKey
-        });
-    }
-
-    //Adding file input to DOM
-    const textarea = editor.targetElm;
-    let inputEl = document.createElement('input');
-    inputEl.type = 'file';
-    inputEl.style.cssText = 'display:none';
-    textarea.parentNode.insertBefore(inputEl, textarea);
-
-    //Create progress element
-    let progressEl = document.createElement('progress');
-    progressEl.max = 100;
-    progressEl.value = 10;
-    progressEl.style.cssText = 'display:none';
-    textarea.parentNode.insertBefore(progressEl, textarea);
-
-    //Creating bucket
-    let bucket = new AWS.S3({
-        params: {
-            Bucket: bucketName
-        }
-    });
-
-
-    inputEl.addEventListener('change', e => {
-        e.preventDefault();
-
-        //Select the file & contentAreaContainer
-        let file = inputEl.files[0],
-            contentAreaContainer = editor.contentAreaContainer;
-
-
-        //If file exists
-        if (file) {
-
-            // Put the progress bar inside content area of TinyMCE
-            contentAreaContainer.parentNode.insertBefore(progressEl, contentAreaContainer);
-            if (progress.bar && typeof progress.bar === 'boolean')
-                progressEl.style.cssText = 'display:block;position: absolute;z-index: 9999;width: 120px;height: 15px;right: 10px;top:45px';
-
-
-            // We are uploading right now
-            inProgress = true;
-
-            let extension = file.name.split('.').pop(),
-                fileName = file.name.split('.' + extension)[0],
-                objKey = (folderName) + '/' + fileName + '-' + Date.now() + '.' + extension,
-                params = {
-                    Key: objKey,
-                    ContentType: file.type,
-                    Body: file,
-                    ACL: 'public-read'
-                };
-
-            bucket.putObject(params).on('httpUploadProgress', progressObj => {
-                let progressPercentage = parseInt((progressObj.loaded / progressObj.total) * 100);
-
-                // Change the value of progressbar. No matter if it's visible or not
-                progressEl.value = progressPercentage;
-
-                // Call the callback function if it's exists
-                if (progress.callback && typeof progress.callback === 'function')
-                    progress.callback(progressPercentage);
-
-            }).send((err, data) => {
-
-                inProgress = false;
-                progressEl.style.cssText = 'display:none';
-
-                if (err) {
-                    if (progress.errorCallback && typeof progress.errorCallback === 'function')
-                        progress.errorCallback(err);
-                } else {
-                    var url = 'https://s3.amazonaws.com/' + bucketName + '/' + objKey;
-
-                    if(progress.successCallback && typeof progress.successCallback === 'function')
-                        progress.successCallback(editor,url);
-
-                }
-            });
-
-        } else {
-
-        }
-    });
-
-
-    editor.addButton('AwsS3UploadButton', {
-        text: buttonText,
-        icon: false,
-        onclick(){
-            if (!inProgress)
-                inputEl.click();
-            else {
-                if (secondFileSelectedBeforeFirstUpload.callback && typeof secondFileSelectedBeforeFirstUpload.callback === 'function')
-                    secondFileSelectedBeforeFirstUpload.callback();
-                else
-                    alert('Progress allready');
-
+      function showDialog() {
+        win = editor.windowManager.open({
+          title: ed.translate('Insert a file from your computer'),
+          width:  500 + parseInt(editor.getLang('uploadfile.delta_width', 0), 10),
+          height: 180 + parseInt(editor.getLang('uploadfile.delta_height', 0), 10),
+          body: [
+            {type: 'iframe',  url: 'javascript:void(0)'},
+            {type: 'textbox', name: 'document[file]', label: ed.translate('Choose a file'), subtype: 'file'},
+            {type: 'textbox', name: 'document[title]', label: ed.translate('Title')},
+            {type: 'container', classes: 'error', html: "<p style='color: #b94a48;'>&nbsp;</p>"}
+          ],
+          buttons: [
+            {
+              text: ed.translate('Insert'),
+              onclick: insertFile,
+              subtype: 'primary'
+            },
+            {
+              text: ed.translate('Cancel'),
+              onclick: ed.windowManager.close
             }
+          ],
+        }, {
+          plugin_url: url
+        });
+
+        // TinyMCE likes pointless submit handlers
+        win.off('submit');
+        win.on('submit', insertFile);
+
+        iframe = win.find("iframe")[0];
+        form = createElement('form', {
+          action: ed.getParam("uploadfile_form_url", "/tinymce_assets"),
+          target: iframe._id,
+          method: "POST",
+          enctype: 'multipart/form-data',
+          accept_charset: "UTF-8",
+        });
+
+        // Might have several instances on the same page,
+        // so we TinyMCE create unique IDs and use those.
+        iframe.getEl().name = iframe._id;
+
+        // Create some needed hidden inputs
+        form.appendChild(createElement('input', {type: "hidden", name: "utf8", value: "✓"}));
+        form.appendChild(createElement('input', {type: 'hidden', name: 'authenticity_token', value: getMetaContents('csrf-token')}));
+
+        var el = win.getEl();
+        var body = document.getElementById(el.id + "-body");
+
+        // Copy everything TinyMCE made into our form
+        var containers = body.getElementsByClassName('mce-container');
+        for(var i = 0; i < containers.length; i++) {
+          form.appendChild(containers[i]);
         }
-    })
 
+        // Fix inputs, since TinyMCE hates HTML and forms
+        var inputs = form.getElementsByTagName('input');
+        for(var i = 0; i < inputs.length; i++) {
+          var ctrl = inputs[i];
 
-});
+          if(ctrl.tagName.toLowerCase() == 'input' && ctrl.type != "hidden") {
+            if(ctrl.type == "file") {
+              ctrl.name = "document[file]";
+              
+              // Hack styles
+              var padding = '0';
+              if(window.navigator.userAgent.match(/(Chrome|Safari)/))
+                padding = '7px 0';
+              
+              tinymce.DOM.setStyles(ctrl, {
+                'border': 0,
+                'boxShadow': 'none',
+                'webkitBoxShadow': 'none',
+                'padding': padding
+              });
+            } else if(ctrl.type == "text") {
+              ctrl.name = "document[title]";
+            }
+          }
+        }
+
+        body.appendChild(form);
+      }
+
+      function insertFile() {
+        if(getInputValue("document[file]") == "") {
+          return handleError('You must choose a file');
+        }
+
+        clearErrors();
+
+        /* Add event listeners.
+         * We remove the existing to avoid them being called twice in case
+         * of errors and re-submitting afterwards.
+         */
+        var target = iframe.getEl();
+        if(target.attachEvent) {
+          target.detachEvent('onload', uploadDone);
+          target.attachEvent('onload', uploadDone);
+        } else {
+          target.removeEventListener('load', uploadDone);
+          target.addEventListener('load', uploadDone, false);
+        }
+
+        form.submit();
+      }
+
+      function uploadDone() {
+        var target = iframe.getEl();
+        if(target.document || target.contentDocument) {
+          var doc = target.contentDocument || target.contentWindow.document;
+          handleResponse(doc.getElementsByTagName("body")[0].innerHTML);
+        } else {
+          handleError("Did not get a response from the server");
+        }
+      }
+
+      function handleResponse(ret) {
+        try {
+          var json = tinymce.util.JSON.parse(ret);
+
+          if(json["error"]) {
+            handleError(json["error"]["message"]);
+          } else {
+            ed.execCommand('mceInsertContent', false, buildHTML(json));
+            ed.windowManager.close();
+          }
+        } catch(e) {
+          console.log(e);
+          handleError('Got a bad response from the server');
+        }
+      }
+
+      function clearErrors() {
+        var message = win.find(".error")[0].getEl();
+
+        if(message)
+          message.getElementsByTagName("p")[0].innerHTML = "&nbsp;";
+      }
+
+      function handleError(error) {
+        var message = win.find(".error")[0].getEl();
+
+        if(message)
+          message.getElementsByTagName("p")[0].innerHTML = ed.translate(error);
+      }
+
+      function createElement(element, attributes) {
+        var el = document.createElement(element);
+        for(var property in attributes) {
+          if (!(attributes[property] instanceof Function)) {
+            el[property] = attributes[property];
+          }
+        }
+
+        return el;
+      }
+
+      function buildHTML(json) {
+        var url = json["document"]["url"];
+        var title = json["document"]["title"];
+        var link = '<a href="' + url + '" title="' + title + '">' + title + '</a>';
+        
+        return link;
+      }
+
+      function getInputValue(name) {
+        var inputs = form.getElementsByTagName("input");
+
+        for(var i in inputs)
+          if(inputs[i].name == name)
+            return inputs[i].value;
+
+        return "";
+      }
+
+      function getMetaContents(mn) {
+        var m = document.getElementsByTagName('meta');
+
+        for(var i in m)
+          if(m[i].name == mn)
+            return m[i].content;
+
+        return null;
+      }
+
+      // Add a button that opens a window
+      editor.addButton('uploadfile', {
+        tooltip: ed.translate('Insert a file from your computer'),
+        icon : 'newdocument',
+        onclick: showDialog
+      });
+
+      // Adds a menu item to the tools menu
+      editor.addMenuItem('uploadfile', {
+        text: ed.translate('Insert a file from your computer'),
+        icon : 'newdocument',
+        context: 'insert',
+        onclick: showDialog
+      });
+    }
+  });
+
+  tinymce.PluginManager.add('uploadfile', tinymce.plugins.UploadFile);
+})();
